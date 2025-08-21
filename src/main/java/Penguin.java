6460084d9f809e8e48ab5c5edd60e1731116557b
JBYoonn <e1398360@u.nus.edu>
2025-08-22 00:58:02 +0800

    Improved the skeletal version of Duke to echo commands entered by the user

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Penguin {
     public static void main(String[] args) {
         String name = "Penguin";
+        String line = "_________________________________";
+
+        System.out.println(line);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(userInput);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
