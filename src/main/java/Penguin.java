b1b51da11f53b325aa893313bf25dc92bd9adf4f
JBYoonn <e1398360@u.nus.edu>
2025-08-22 00:20:20 +0800

    Replaced Duke.java with Penguin.java

---
+++ src/main/java/Penguin.java
@@ -0,0 +1,8 @@
+public class Penguin {
+    public static void main(String[] args) {
+        String name = "Penguin";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
