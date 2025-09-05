e6db5a3c5fa8ca09b464e1839ab92fa55b984944
JBYoonn <e1398360@u.nus.edu>
2025-09-05 22:03:19 +0800

    Add TaskList.java

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskList {
+    private final List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
+
+    public Task removeAt(int index) {
+        return this.tasks.remove(index);
+    }
+
+    public int size() {
+        return this.tasks.size();
+    }
+
+    public Task get(int index) {
+        return this.tasks.get(index);
+    }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public boolean isValidIndex(int index) {
+        return index > 0 && index <= tasks.size();
+    }
 }
