5a321adaa2842f718861aebf38aa295800b26f34
JBYoonn <e1398360@u.nus.edu>
2025-09-05 22:04:22 +0800

    Add DeleteCommand.java

--- src/main/java/DeleteCommand.java
+++ src/main/java/DeleteCommand.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.IOException;
 
-public class DeleteCommand {
+public class DeleteCommand extends Command {
+    public DeleteCommand(String input) {
+        super(input);
+    }
+
+    @Override
+    public boolean execute(TaskList tasks, Ui ui, Storage storage) throws PenguinException {
+        int id = Parser.parseIndex(input, 7);
+        if (!tasks.isValidIndex(id)) {
+            ui.showBadId();
+            return false;
+        }
+
+        Task removed = tasks.removeAt(id - 1);
+        ui.showRemovedTask(removed, tasks.size());
+
+        try {
+            storage.save(tasks.getTasks());
+        } catch (IOException e) {
+            // user might have deleted file mid-run
+        }
+
+        return false;
+    }
 }
