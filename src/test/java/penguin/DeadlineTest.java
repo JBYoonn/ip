6c1cd268dbde56d613124e7c9b1d1ec5f0766456
JBYoonn <e1398360@u.nus.edu>
2025-09-12 01:19:24 +0800

    Fix coding standards for tests

--- src/test/java/penguin/DeadlineTest.java
+++ src/test/java/penguin/DeadlineTest.java
@@ -1,10 +1,11 @@
 package penguin;
 
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
     @Test
