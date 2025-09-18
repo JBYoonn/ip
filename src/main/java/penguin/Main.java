d9bdd1ca4ae72468ce5ffca7f2fc1dafd63896ad
JBYoonn <e1398360@u.nus.edu>
2025-09-19 03:45:30 +0800

    Improve coding standard

--- src/main/java/penguin/Main.java
+++ src/main/java/penguin/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setPenguin(penguin);  // inject the Penguin instance
+            fxmlLoader.<MainWindow>getController().setPenguin(penguin); // inject the Penguin instance
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/penguin/Penguin.java
+++ src/main/java/penguin/Penguin.java
@@ -1,7 +1,6 @@
 package penguin;
 
 import java.io.IOException;
-
 import java.util.ArrayList;
 import java.util.List;
 
